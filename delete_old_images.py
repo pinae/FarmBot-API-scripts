778284eb2a0f24645ed4a7c9a1e1448bb030f473
pinae <pina@pinae.net>
2021-03-30 17:37:41 +0200

    Deleting images older than one day and 12 hours

--- delete_old_images.py
+++ delete_old_images.py
@@ -9,10 +9,12 @@ images = response.json()
 print(json.dumps(images, indent=2))
 now = datetime.now(tz=pytz.timezone("Europe/Berlin"))
 for image in images:
-    create_date = datetime.strptime(images[-1]["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
+    create_date = datetime.strptime(image["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
     create_date = create_date.replace(tzinfo=pytz.timezone("Europe/Berlin"))
     print(str(create_date) + " < " + str(now - timedelta(days=1, hours=12)) +
           " = " + str(create_date < now - timedelta(days=1, hours=12)))
     if create_date < now - timedelta(days=1, hours=12):
         print("Deleting because of old age: " + str(image["id"]))
-        # requests.delete('https://my.farmbot.io/api/images/' + str(image["id"]), headers=get_headers())
+        requests.delete('https://my.farmbot.io/api/images/' + str(image["id"]), headers=get_headers())
+    else:
+        print("Keeping image: " + str(image["id"]))
